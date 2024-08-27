d2ca690a4977fcefcca7793c0722d834e4d67149
MAURICE ODHIAMBO OMBEWA <124437425+MauriceOmbewa@users.noreply.github.com>
2024-08-27 14:50:23 +0300

    feat: add abs.js

---
+++ abs.js
@@ -0,0 +1,15 @@
+function isPositive(arg){
+    if (arg > 0){
+        return true
+    } else {
+        return false
+    }
+}
+
+function abs(arg){
+    if (arg >= 0){
+        return arg
+    } else {
+        return arg * (-1);
+    }
+}

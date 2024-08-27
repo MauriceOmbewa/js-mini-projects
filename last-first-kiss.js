52bc7c00019349901705f698c7a3f584cfd8a77d
MAURICE ODHIAMBO OMBEWA <124437425+MauriceOmbewa@users.noreply.github.com>
2024-08-27 11:17:13 +0300

    feat: update last-first-kiss.js

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,7 +1,17 @@
-function first(arg){
+function kiss(arg){
     const fir = arg[0]
     const las = arg[arg.length-1]
 
     const arr = [fir, las]
     return arr
+}
+
+function last(arg){
+    const las = arg[arg.length-1]
+    return las
+}
+
+function first(arg){
+    const fir = arg[0]
+    return fir
 }

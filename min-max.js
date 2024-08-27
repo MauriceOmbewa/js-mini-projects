4e902d01c234e262b64d2c4ff2c009db24e8c925
MAURICE ODHIAMBO OMBEWA <124437425+MauriceOmbewa@users.noreply.github.com>
2024-08-27 14:56:28 +0300

    feat: add min-max.js

---
+++ min-max.js
@@ -0,0 +1,15 @@
+function max(arg, arg2){
+    if (arg > arg2){
+        return arg
+    } else if (arg < arg2){
+        return arg2
+    }
+}
+
+function min(arg, arg2){
+    if (arg < arg2){
+        return arg
+    } else if (arg > arg2){
+        return arg2
+    }
+}

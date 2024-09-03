6698c2c23ebefa13c7a46cf79a45652220c4dabf
MAURICE ODHIAMBO OMBEWA <124437425+MauriceOmbewa@users.noreply.github.com>
2024-09-03 14:58:47 +0300

    Update count-leap-year.js

--- count-leap-year.js
+++ count-leap-year.js
@@ -2,7 +2,7 @@ function countLeapYears(date) {
     let year = date.getFullYear(); // Get the year from the Date object
     let count = 0;
 
-    for (let y = 1; y <= year; y++) {
+    for (let y = 1; y < year; y++) {
         if ((y % 4 === 0 && y % 100 !== 0) || (y % 400 === 0)) {
             count++;
         }
